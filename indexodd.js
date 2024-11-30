81f9e62ba1f09f41b2015cb2555ed525f8efd9c9
madhumitaamaharaj <madhumitaamaharaj7@gmail.com>
2024-11-30 18:34:56 +0530

    Add evenOdd function to check even and odd numbers

---
+++ indexodd.js
@@ -0,0 +1,11 @@
+// Function to check if a number is even or odd
+function evenOdd(num) {
+    if (num % 2 === 0) {
+        return true; 
+    } else {
+        return false; 
+    }
+}
+
+console.log(evenOdd(4)); 
+console.log(evenOdd(5)); 
